c4d3621eae62a2f670b11e4ad1b43e20a5359bfd
GakuruUri <urigakuru@icloud.com>
2024-06-30 17:00:37 +0300

    Day 34 of 100 days of code.

--- guess_number.py
+++ guess_number.py
@@ -1,40 +1,30 @@
-" This is a guess the number game"
+# This is as guess the number game
 import random
 
 guessTaken = 0
 
-print("Hello! what is your name?")
+print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1, 20)
-print(f"Well,{myName}, I am thinking of a number between 1 and 20.")
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 for guessTaken in range(6):
-    print("Take a guess.")
+    print('Take a guess')
     guess = input()
     guess = int(guess)
 
     if guess < number:
-        print("your guess is too low.")
+        print('Your guess is too low.')
 
-    if guess > number:
-        print("Your guess is too high")
+    elif guess > number:
+        print('Your guess is too high.')
 
-    if guess == number:
+    else:
         break
 
 if guess == number:
-    guessTaken = str(guessTaken + 1)
-    print(f"Good job, {myName}! You guessed my number in {guessTaken} guesses!")
-
+    guessTaken = str(guessTaken + 1 )
+    print('Good Job, ' + myName + '! You guessed my number in ' + guessTaken + ' guesses!')
 if guess != number:
     number = str(number)
-    print("Nope. The number I was thinking of was '+ number + '.")
-
-
-
-
-
-
-
-
